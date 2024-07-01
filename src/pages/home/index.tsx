6a9f0c8225be63f284796e573446dac39874510a
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-07-01 00:19:07 -0300

    ajustes...

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,7 @@ import Projetos from "../../components/Projetos"
 import Skills from "../../components/Skills"
 import styles from './Home.module.scss' 
 import { ToogleBtnContext } from "../../context/toogleBtn"
+import { RxHamburgerMenu } from "react-icons/rx";
 import NavBar from "../../components/NavBar"
 
 const Home = () => {
@@ -13,7 +14,7 @@ const Home = () => {
   return (
     <>
       <main className={styles.content}>
-        <button onClick={()=>setState(!state)} className={`${styles.toggleBtn} ${styles.off}`}>X</button>
+        <button onClick={()=>setState(!state)} className={`${styles.toggleBtn} ${styles.off}`}>{state ? 'X' : <RxHamburgerMenu/>}</button>
         <NavBar/>
         <Header/>
         <Skills/>
